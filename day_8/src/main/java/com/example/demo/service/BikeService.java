0e5f810be718b70269bdab5b8afe45bf6ce6096e
727721euec041 <108514278+727721euec041@users.noreply.github.com>
2023-09-01 10:37:00 +0530

    day_8 files uploaded

---
+++ day_8/src/main/java/com/example/demo/service/BikeService.java
@@ -0,0 +1,11 @@
+package com.example.demo.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BikeService {
+	public static void main(String[] args) {
+		SpringApplication.run(BikeService.class, args);
+	}
+}
